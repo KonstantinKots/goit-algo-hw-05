ef673eeb8fbaaa5f49e9d75ef4c8ff703901744e
Konstantin K <kim9@i.ua>
2026-03-05 20:39:46 +0200

    second task

---
+++ second_task_hw5.py
@@ -0,0 +1,24 @@
+
+import re
+from typing import Callable, Iterator
+#Функція для аналізу сторки на наявність чисел
+def gen_num(text: str):
+    """Генератор чисел з тексту.
+    Ловить числа з крапкою"""
+    for num in re.findall(r"\d+\.\d+", text):
+        yield float(num)
+
+#Функція обрахування загального прибутку
+def sum_profit(text: str, func: Callable [[str], Iterator[float]]) -> float:
+    """Обчислює суму чисел, які повертає функція func."""
+    return sum(func(text))
+
+def main():
+    text = """Загальний дохід працівника складається з декількох
+     частин: 1000.01 як основний дохід, доповнений додатковими
+     надходженнями 27.45 і 324.00 доларів."""
+    total_income = sum_profit(text, gen_num)
+    print(f"Загальний дохід: {total_income:.2f}")
+
+if __name__ == '__main__':
+    main()
