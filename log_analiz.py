6175181c2b05370efa0efb0c783bf9e352af45d0
Konstantin K <kim9@i.ua>
2026-03-24 20:54:32 +0200

    final hw_task_03

--- log_analiz.py
+++ log_analiz.py
@@ -1,44 +1,66 @@
 from datetime import datetime
 from pathlib import Path
+from collections import Counter
 import re
-#Скрипт для виводу стати логування за різними рівнями
-#шлях до файлу - обєкт Паз
+import sys
+# Скрипт для виводу стати логування за різними рівнями
 
-#Функція для парсингу логів
+# Функція для парсингу логів
 def parse_log_line(line: str) -> dict:
     log_dic = {}
     line_log = re.match(r'^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) (\w+) (.+)$', line)
     if line_log:
         log_dic["date_time"] = datetime.strptime(line_log.group(1), "%Y-%m-%d %H:%M:%S")
-        log_dic["level_log"] = line_log.group(2)
+        log_dic["level"] = line_log.group(2)
         log_dic["text"] = line_log.group(3)
     return log_dic
 
-#Функція для завантаження логів з файлу???
+# Функція для завантаження логів з файлу
 def load_logs(file_path: str) -> list:
     try:
         with open (file_path, 'r', encoding='utf-8')  as file:
-            return [line.strip() for line in file if line.strip().parse_log_line()]    
+            return [parse_log_line(line.strip()) for line in file if line.strip()]    
     except FileNotFoundError:
-        return [] #список словників
+        return []
 
-#Функція для фільтрації логів з файлу
+# Функція для фільтрації логів за рівнем
 def filter_logs_by_level(logs: list, level: str) -> list:
-    return list(filter())
+    return [log for log in logs if log.get('level') == level]
 
-
-# #Функція для підрахунку логів з файлу
+# Функція для підрахунку записів логів
 def count_logs_by_level(logs: list) -> dict:
+    counts = Counter(log['level'] for log in logs).most_common()
+    return counts
 
-# #Функція для форматування та виводу результатів 
+# Функція для форматування та виводу результатів 
 def display_log_counts(counts: dict):
+    print(f"\n{'Рівень логування':<18} | {'Кількість'}")
+    print('-' * 45)
+    for level, count in counts:
+        print(f"{level:<18} | {count}")
 
 def main():
-    file_path = Path('logfile.log')
-    parse_log_line()
-    print(log_dic)
+    try:
+        if len(sys.argv) > 1:
+            file_path = Path(sys.argv[1])
+            logs = load_logs(file_path)
+            counts = count_logs_by_level(logs)
+            display_log_counts(counts)
 
+            if len(sys.argv) > 2:
+                level = sys.argv[2]
+                filter_logs = filter_logs_by_level(logs, level)
 
+            print(f"\nДеталі логів для рівня {level}: ")
+            if filter_logs:
+                for log in filter_logs:
+                    print(f"{log['date_time']} - {log['text']}")
+            else:
+                print("No logs for this level")
+        else:
+            print("Use: name_script.py <log_file> [level]")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
